267333676314fdaf60da2ed379a38766ecb34557
Evgeny Pekerman <geka.kud@gmail.com>
2023-07-23 17:44:43 +0000

    add line graph

--- bee_frontend/dashboard.py
+++ bee_frontend/dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pymongo
-
+from datetime import datetime
+import numpy as np
 
 mongo_uri = "mongodb+srv://bee_admin:bee_admin_pass@atlascluster.d85negs.mongodb.net/?retryWrites=true&w=majority"
 
@@ -27,11 +28,34 @@ def get_all_device_ids():
 
     return device_ids
 
+def convert_and_get_weight_data():
+    # Retrieve all records from the collection
+    cursor = collection.find({})
+
+    # Extract timestamp and weight from each record and store in lists
+    timestamps = []
+    weights = []
+    for record in cursor:
+        timestamp = datetime.fromisoformat(record['timestamp'])
+        weight = record['weight']
+        timestamps.append(timestamp)
+        weights.append(weight)
+
+    # Convert lists to NumPy arrays
+    timestamps_array = np.array(timestamps)
+    weights_array = np.array(weights)
+
+    return timestamps_array, weights_array
+
 with st.sidebar:
     all_hives = get_all_device_ids()
     hive_selection = st.selectbox('Choose hive', options=all_hives)
     
-    if hive_selection:
-        hive_data = load_records_by_device_id(hive_selection)
-        print(hive_data)
+if hive_selection:
+    hive_data = load_records_by_device_id(hive_selection)
+    all_records_list = convert_and_get_weight_data()
+    # print(hive_data)
+    timestamps, weights = convert_and_get_weight_data()
+
+    st.line_chart(weights, use_container_width=True)
     
