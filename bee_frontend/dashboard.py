76f46366327142f41fd766eea98cc9aa51ec92be
Evgeny Pekerman <geka.kud@gmail.com>
2023-07-23 17:02:12 +0000

    get real data

--- bee_frontend/dashboard.py
+++ bee_frontend/dashboard.py
@@ -1,9 +1,37 @@
 import streamlit as st
+import pymongo
+
 
 mongo_uri = "mongodb+srv://bee_admin:bee_admin_pass@atlascluster.d85negs.mongodb.net/?retryWrites=true&w=majority"
 
-def load_data():
-    pass
+# Connect to MongoDB
+client = pymongo.MongoClient(mongo_uri)
+db = client['bee_db_dev']  # Replace 'mydatabase' with your database name
+collection = db['bee_metadata_col']  # Replace 'mycollection' with your collection name
+
+def load_records_by_device_id(device_id):
+    # Define the query filter to find records with the given device_id
+    query = {"device_id": device_id}
+
+    # Use the find method with the query filter to retrieve the matching records
+    cursor = collection.find(query)
+
+    # Convert the cursor to a list of dictionaries (JSON-like objects)
+    records = list(cursor)
+
+    return records
+
+def get_all_device_ids():
+    # Use the distinct method to get all unique device_id values
+    device_ids = collection.distinct("device_id")
+
+    return device_ids
 
 with st.sidebar:
-    hive_selection = st.selectbox('Choose hive')
+    all_hives = get_all_device_ids()
+    hive_selection = st.selectbox('Choose hive', options=all_hives)
+    
+    if hive_selection:
+        hive_data = load_records_by_device_id(hive_selection)
+        print(hive_data)
+    
